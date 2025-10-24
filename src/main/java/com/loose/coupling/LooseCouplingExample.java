ed19cf9001732b28b38cb5fe9860cda570adde29
keremcanozcan35 <keremcanozcan35@gmail.com>
2025-10-24 17:59:51 +0300

    35-challenge
    challenge ioc is done

--- src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -1,10 +1,13 @@
 package com.loose.coupling;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class LooseCouplingExample {
     public static void main(String[] args) {
-        UserDataProvider databaseProvider = new UserDatabaseProvider();
-        UserManager userManagerWithDB = new UserManager(databaseProvider);
-        System.out.println(userManagerWithDB.getUserInfo());
+       // UserDataProvider databaseProvider = new UserDatabaseProvider();
+       // UserManager userManagerWithDB = new UserManager(databaseProvider);
+       // System.out.println(userManagerWithDB.getUserInfo());
 
         UserDataProvider webServiceProvider = new WebServiceDataProvider();
         UserManager userManagerWithWS = new UserManager(webServiceProvider);
@@ -13,6 +16,11 @@ public class LooseCouplingExample {
         UserDataProvider newDatabaseProvider = new NewDatabaseProvider();
         UserManager userManagerWithNewDB = new UserManager(newDatabaseProvider);
         System.out.println(userManagerWithNewDB.getUserInfo());
+
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationChallange.xml");
+        UserManager userManagerFromSpring = (UserManager) context.getBean("userManagerWithUserDataProvider");
+        System.out.println("buneeeee"+userManagerFromSpring.getUserInfo());
     }
 }
 //deneme
--- src/main/java/com/loose/coupling/UserManager.java
+++ src/main/java/com/loose/coupling/UserManager.java
@@ -3,6 +3,7 @@ package com.loose.coupling;
 public class UserManager {
     private UserDataProvider userDataProvider;
 
+
     public UserManager(UserDataProvider userDataProvider) {
         this.userDataProvider = userDataProvider;
     }
